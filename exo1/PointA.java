de129e511855ab5d921e95ff1475f7f503f66da7
agraire <94527096+agraire@users.noreply.github.com>
2022-11-14 09:00:28 +0000

    Initial commit

---
+++ exo1/PointA.java
@@ -0,0 +1,9 @@
+package exo1;
+
+public class PointA extends Point {
+
+  public void afficher(){
+    System.out.println("Le point a pour coordonnées en x : " + getX() + " et en y : " + getY() + "." );
+  }
+  
+}
---
+++ exo2/PointNom.java
@@ -0,0 +1,24 @@
+package exo2;
+
+public class PointNom extends Point {
+  public PointNom(int x, int y, String nom){
+    setPointnom(x, y, nom);
+  }
+  
+  private String nom;
+
+  public void setPointNom(int x, int y, String nom){
+    setPoint(x, y);
+    setNom(nom);
+  }
+
+  public void setNom(String nom){
+    this.nom = nom;
+  }
+
+  public void affCoordsNom(){
+    afficheCoords();
+    System;out.println("Nom = " + nom);
+  }
+  
+}
